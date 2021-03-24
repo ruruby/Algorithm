357655895b492178965c0e14262c82acb22d1654
ruruby <rubyzxc@naver.com>
2021-03-24 13:40:46 +0900

    upgrade

--- recursive function/ex02_power3.c
+++ recursive function/ex02_power3.c
@@ -4,8 +4,8 @@
 double power(double x, int n) {
 	printf("power호출됨 ---> x : %f, n : %d\n", x, n);
 	if (n == 1) return x;//n이 1일 경우
-	if (n % 2 == 0) return power(x*x, n / 2);//n이 짝수일 경우
-	else if (n % 2 == 1)return x * power(x, n - 1);//n이 홀수일 경우
+	if (n % 2 == 0) return power(x*x, n/2);
+	else if (n % 2 == 1) return x*power(x*x, (n-1)/2);
 }
 
 int main(void) {
@@ -13,7 +13,7 @@ int main(void) {
 	int n;
 	printf("x의 n제곱 계산 , x와 n을 입력하시오 : ");
 	scanf("%lf %d", &x, &n);
-	printf("%f", power(x, n));
+	printf("%f\n", power(x, n));
 }
 
 
