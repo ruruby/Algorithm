229455cca127514f4ece5126d9dfb9d9ffef04b6
ruruby <rubyzxc@naver.com>
2021-03-24 12:57:23 +0900

    upgrade

---
+++ recursive function/ex02_power2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+double power(double x, int n) {
+	printf("power호출됨\n");
+	if (n == 1) return x;//n이 1일 경우
+	if (n % 2 == 0) return power(x*x, n / 2);//n이 짝수일 경우
+	else if (n % 2 == 1)return x * power(x, n - 1);//n이 홀수일 경우
+}
+
+int main(void) {
+	double x;
+	int n;
+	printf("x의 n제곱 계산 , x와 n을 입력하시오 : ");
+	scanf("%lf %d", &x, &n);
+	printf("%f", power(x, n));
+}
+
+
